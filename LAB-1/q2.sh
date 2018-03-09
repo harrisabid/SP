91fa3a1e0f01f8ea1f0781bf1e90da50f884e346
harrisabid <36966027+harrisabid@users.noreply.github.com>
2018-03-09 19:50:27 +0500

    Update q2.sh

--- LAB-1/q2.sh
+++ LAB-1/q2.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
- 
-set ls -li $1
 
+user2=$4
+file2=$3
+user1=$2
+file1=$1
 
-echo "owner: " $6
-echo "group: " $7
-echo "permissions: " $4
+set `ls -li $1` 
+
+owner=$5
+echo "Owner: " $owner
+echo "Group: " $4
+echo "Permissions: " $2
+shift
+echo "Filename: " $9
+if [ $owner = $user1 ] 
+	then
+	echo "Cheating: " 0
+else
+	echo "Cheating: " 1
+fi
+
+if ( test ${#file2} -gt 0 && test ${file2} != $user1 )
+then
+	set `ls -li ${file2[0]}` 
+
+	echo "Owner: " $5
+	echo "Group: " $4
+	echo "Permissions: " $2
+	shift
+	echo "Filename: " $9
+	diff -c $file1 $9
+	if [ $? -eq 0 ]
+	then 
+	echo "NO cheating"
+	else
+	echo "Cheating"
+	fi
+fi
